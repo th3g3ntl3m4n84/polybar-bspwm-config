afd4ad623d7be37802d6581356a8df9cd976b8c1
th3g3ntl3m4n <jpfguedes@gmail.com>
2024-08-09 15:44:48 -0400

    Add files via upload

---
+++ shapes/scripts/battery.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Coloque aqui o local que identifica sem seu dispositivo está carregando ou descarregando
+# IMPORTANTE ! A saída deve ser em binário, caso contrário resultará em erro!
+is_plugged=$(cat /sys/class/power_supply/ADP1/online)
+
+# Coloque aqui o local que identifica o número relativo a porcentagem da bateria de seu dispositivo
+battery_level=$(cat /sys/class/power_supply/BAT1/capacity)
+
+# EXTRA! Altere os ícones para qualquer um de sua preferência, assim como os valores da %
+# EXTRA! Customize a cor do ícone começando com %{F#} e fechando com %{F-}
+if [ "$is_plugged" -eq 1 ]; then
+    icon="%{F#F0C674}%{F-}"
+else
+    if [ "$battery_level" -le 15 ]; then
+        icon="%{F#F0C674}%{F-}"
+    elif [ "$battery_level" -le 25 ]; then
+        icon="%{F#F0C674}%{F-}"
+    elif [ "$battery_level" -le 60 ]; then
+        icon="%{F#F0C674}%{F-}"
+    elif [ "$battery_level" -le 90 ]; then
+        icon="%{F#F0C674}%{F-}"
+    else
+        icon="%{F#F0C674}%{F-}"
+    fi
+fi
+
+echo "$icon $battery_level%"
+
